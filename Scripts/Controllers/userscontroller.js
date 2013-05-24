bdddd4ad1a178dccb0dbcfc46fc324132b425da6
Federico Rota <federico@codiceplastico.com>
2013-05-24 15:26:51 +0200

    AngularYeoman

---
+++ Scripts/Controllers/userscontroller.js
@@ -0,0 +1,15 @@
+function UsersController($scope) {
+	$scope.users = [{
+		Name: 'Mario',
+		PreferredCategory: 'Ottica'
+	},
+	{
+		Name: 'Anna',
+		PreferredCategory: 'Cancelleria'
+	}];
+	function CheckUsers(){
+	console.log("user");
+	}
+	// body...
+}
+ 
