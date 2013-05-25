20f67ca877486317d291bbf672ab8a12ae95a9b7
Federico Rota <federico.rota01@gmail.com>
2013-05-25 09:53:39 +0200

    refactoring, express, fix header table

---
+++ app/scripts/controllers/itemcontroller.js
@@ -0,0 +1,30 @@
+var itemsController = function ($scope) {
+	$scope.items = [
+		{
+		Name: "Trasmissione",
+		Value: 22,
+		Categoria: "Auto"
+	},
+	{
+		Name: "Telescopio",
+		Value: 3.4,
+		Categoria: "Ottica"
+	},
+	{
+		Name: "Penna",
+		Value: 8.4,
+		Categoria: "Cancelleria"
+	}
+	];
+	$scope.ordering = 'Value';
+	$scope.$watch('query', function(itm){
+		$scope.searchItem = itm;
+	});
+	$scope.clear = function(){
+		$scope.searchItem = 
+		$scope.query = "";
+	};
+	$scope.detail = function(item){
+		alert('Prezzo '+item.Name+': '+item.Value);
+	};
+};
---
+++ web-server.js
@@ -0,0 +1,12 @@
+var express = require("express"),
+app = express(),
+port = parseInt(process.env.PORT, 10) || 8080;
+
+app.configure(function(){
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(express.static(_dirname + '/'));
+	app.use(express.router);
+});
+ app.listen(port);
+ console.log('Now serving the app at http://localhost:'+ port + '/app');
