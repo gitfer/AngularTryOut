b3258887251b9cb3b226512698c51c0669565cfc
Federico Rota <federico.rota01@gmail.com>
2013-06-02 15:28:19 +0200

    Added conditional test

--- test/spec/controllers/conditionals.js
+++ test/spec/controllers/conditionals.js
@@ -16,7 +16,8 @@ describe('Controller: ConditionalsCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('emitSoundIfPirate should return sound if is pirate', function () {
+    expect(scope.emitSoundIfPirate(true)).toBe('Arrrr');
+    expect(scope.emitSoundIfPirate(false)).toBe('');
   });
 });
